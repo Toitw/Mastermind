de117f3990f968e394cc319906ffc5e8721f8c3c
juroga_new <juanjrgast@gmail.com>
2022-11-21 18:19:45 +0100

    Machine codemaker vs player codebreaker done. Started player codemaker vs machine codebreaker

--- mastermind.rb
+++ mastermind.rb
@@ -17,12 +17,20 @@ class Human_solver
     end
 end
 
+class HumanCodemaker
+    def initialize
+        puts "Create the code using digits from 1 to 6 only and 4 digits length"
+        @human_code = gets.chomp.to_i.digits.reverse
+    end
+end
+
 class Game
-    attr_reader :machine_code, :guess_arr, :right_indexes, :round_counter
+    attr_reader :machine_code, :guess_arr, :right_indexes, :round_counter, :human_code
 
     @@end_game = false
 
     def initialize
+        @human_code = HumanCodemaker.new.human_code
         @machine_code = MachineCodeMaker.new.machine_code
         @guess_arr = []
         @intro_message = Display.new.human_set_code
@@ -30,9 +38,18 @@ class Game
         @code_history_arr = []
         @code_guess = Human_solver.new.code_guess
     end
+
+    # gets the code from human with restrictions (only digits 1-6 and 4 digit length)
     def get_human_code
         @code_guess = gets.chomp.to_i
         @guess_arr = @code_guess.digits.reverse
+        if @guess_arr.length < 4 || @guess_arr > 4
+            puts "The code has to contain 4 digits, try again"
+            get_human_code
+        elsif (@guess_arr - [0, 7, 8, 9]).length < 4
+            puts "The code can only contain digits from 1 to 6 both included. Try again"
+            get_human_code
+        end
     end
 
     def check_number
@@ -57,11 +74,11 @@ class Game
 
     def game_over
         if @right_indexes == 4
-            puts "You broke the code!"
+            puts "\nCongratulations, you broke the code!"
             @@end_game = true
             return
-        elsif @round_counter == 6
-            puts "You lose"
+        elsif @round_counter == 12
+            puts "\nYou lose. The code was #{@machine_code}"
             @@end_game = true
             return
         else
@@ -77,15 +94,24 @@ class Game
         game_over
     end
 
-    def game_loop
+    def play
         puts @machine_code
         while @@end_game == false do
             round
         end
+        play_again
     end
 
     def round_message
-        puts "Round ##{@round_counter}: Enter a 4 digit code. Digits have to be from 1-6"
+        puts "\nRound ##{@round_counter}: Enter a 4 digit code. Digits have to be from 1-6"
+    end
+
+    def play_again
+        puts "\nWould you like to play again? Press 'y' if you want to continue, else press any other key."
+        if gets.chomp == "y"
+            @@end_game = false
+            play
+        end
     end
 
 end
@@ -98,5 +124,4 @@ class MachineCodeMaker
     end
 end
 
-new_game = Game.new
-new_game.game_loop
+Game.new.human_code
